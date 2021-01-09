7db90842328f8789b45c18863fdd5d487d70391e
cxvdy <77176895+cxvdy@users.noreply.github.com>
2021-01-09 02:26:14 -0500

    Update shehacks.py
    - Changed "random" to "value" cus a bit too similar to a function
    - Added loop for when start >= end, until they give a valid range. 
    - Runs perfectly from what I tested

--- shehacks.py
+++ shehacks.py
@@ -2,35 +2,33 @@ import random
 
 #The user chooses the numbers for the range 
 print("Choose a range of numbers")
-start = int(input("Enter first number: "))
-end = int(input("Enter last number: "))
+while True:
+  start = int(input("Enter first number: "))
+  end = int(input("Enter last number: "))
 
 #Exit app if user inputs data format 
-if(start > end): 
-  print("Incorrect range")
-  exit()
-
-#generate random number 
-value = random.randint(start,end)
-
-#run loop until number is correctly guessed 
-while True:
-  userguess = (input("Guess random number: "))
-
-  #checks if input number is of type integer
-  if (userguess.isdigit() is False):
-    print("Error. Only integers are allowed")
-
-  elif (int(userguess) < value):
-    print("Incorrect. The number is higher")
-
-  elif (int(userguess) > value):
-    print("Incorrect. The number is lower")
-
+  if(start >= end): 
+    print("First number should be lower than the last number!")
   else:
-    print("Congratulations. You guessed the number!")
-    break
+    #generate random number 
+    value = random.randint(start,end)
+    #run loop until number is correctly guessed 
+    while True:
+      userguess = (input("Guess random number: "))
+      #checks if input number is of type integer
+      if (userguess.isdigit() is False):
+        print("Error. Only integers are allowed")
 
+      elif (int(userguess) < value):
+        print("Incorrect. The number is higher")
+
+      elif (int(userguess) > value):
+        print("Incorrect. The number is lower")
+
+      else:
+        print("Congratulations. You guessed the number!")
+        break
+    break
 
 
 
