bf43fa1539fad32c01987efaac0f82dbf4819a0b
chityalasarayu <77184613+chityalasarayu@users.noreply.github.com>
2021-01-09 00:34:29 -0500

    Update shehacks.py

--- shehacks.py
+++ shehacks.py
@@ -1,18 +1,23 @@
 import random
 
+#The user chooses the numbers for the range 
 print("Choose a range of numbers")
 start = int(input("Enter first number: "))
 end = int(input("Enter last number: "))
 
+#Exit app if user inputs data format 
 if(start > end): 
   print("Incorrect range")
   exit()
 
+#generate random number 
 random = random.randint(start,end)
 
+#run loop until number is correctly guessed 
 while True:
   userguess = (input("Guess random number:"))
 
+  #checks if input number is of type integer
   if (userguess.isdigit() is False):
     print("Error. Only integers are allowed")
 
