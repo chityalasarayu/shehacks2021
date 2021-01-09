1b0a954a73368ec689add2a64b0cfc646b2d8c60
cxvdy <77176895+cxvdy@users.noreply.github.com>
2021-01-09 03:33:48 -0500

    Update shehacks.py

--- shehacks.py
+++ shehacks.py
@@ -1,34 +1,57 @@
 import random
+print("GUESS RANDOM NUMBER")
+while True:#to restart program if requested
+  
+  #The user chooses the numbers for the range 
+  print()
+  print("Choose a range of numbers")
+  
+  #run loop until user inputs valid range
+  while True:
+    start = int(input("Enter minimum: "))
+    end = int(input("Enter maximum: "))
+    print()
+    
+    if(start >= end): 
+      print("Minimum should be lower than the maximum. Try again!")
+      print()
+    else:
+      #generate random number 
+      value = random.randint(start,end)
+      
+      #run loop until number is correctly guessed 
+      while True:
+        userguess = (input("Guess the selected number: "))
+        print()
+        
+        #checks if input number is of type integer
+        if (userguess.isdigit() is False):
+          print("Error. Only integers are allowed")
 
-#The user chooses the numbers for the range 
-print("Choose a range of numbers")
-while True:
-  start = int(input("Enter first number: "))
-  end = int(input("Enter last number: "))
+        elif (int(userguess) < value):
+          print("Incorrect. The number is higher")
 
-#Exit app if user inputs data format 
-  if(start >= end): 
-    print("First number should be lower than the last number!")
+        elif (int(userguess) > value):
+          print("Incorrect. The number is lower")
+
+        else:
+          print("Congratulations. You guessed the number!")
+
+          break
+      break
+   
+  #prompts to run program again
+  while True:
+    answer = str(input('Run again? (Yes/No): '))
+    if answer in ('Yes', 'No'):
+      break
+    print("Invalid answer.")
+
+  if answer == 'Yes':
+    continue
   else:
-    #generate random number 
-    value = random.randint(start,end)
-    #run loop until number is correctly guessed 
-    while True:
-      userguess = (input("Guess random number: "))
-      #checks if input number is of type integer
-      if (userguess.isdigit() is False):
-        print("Error. Only integers are allowed")
-
-      elif (int(userguess) < value):
-        print("Incorrect. The number is higher")
-
-      elif (int(userguess) > value):
-        print("Incorrect. The number is lower")
-
-      else:
-        print("Congratulations. You guessed the number!")
-        break
+    print()
+    print("Goodbye")
     break
-
 
 
